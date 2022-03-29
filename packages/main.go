a67236a676b16bd67a2fb53e6774c1b700f81ff3
Kranthi Rachakonda <kranthikumar.rachakonda@lowes.com>
2022-03-29 19:07:45 -0400

    sample concurrency with goroutines

--- packages/main.go
+++ packages/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"packages/store"
+	"packages/store/cart"
 )
 
 func main() {
 	product := store.NewProduct("Kayak", "WaterSports", 275.99)
+	cart := cart.Cart{
+		CustomerName: "Kraz",
+		Products:     []store.Product{*product},
+	}
 	fmt.Println("Name:", product.Name)
 	fmt.Println("Category:", product.Category)
 	fmt.Println("Price:", product.Price())
