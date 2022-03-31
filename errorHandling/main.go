c9112fa2bb588f177eebdb669cf061e3f00243ef
Kranthi Rachakonda <kranthikumar.rachakonda@lowes.com>
2022-03-31 11:46:34 -0400

    Updated with async code

--- errorHandling/main.go
+++ errorHandling/main.go
@@ -3,9 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	categories := []string{"Watersports", "Chess"}
-	for _, ctg := range categories {
-		total := Products.TotalPrice(ctg)
-		fmt.Println("Category:", ctg, "Total:", ToCurrency(total))
+	categories := []string{"Watersports", "Chess", "Running"}
+	channel := make(chan ChanMessage, 10)
+	go Products.TotalPriceAsync(categories, channel)
+	for msg := range channel {
+		if msg.CategoryError == nil {
+			fmt.Println(msg.Category, "total:", ToCurrency(msg.Total))
+		} else {
+			fmt.Println(msg.Category, "(no such category)")
+		}
 	}
+	/* 	for _, ctg := range categories {
+		total, err := Products.TotalPrice(ctg)
+		if err == nil {
+			fmt.Println("Category:", ctg, "Total:", ToCurrency(total))
+		} else {
+			fmt.Println("Category:", ctg, "(does'nt exist)")
+		}
+
+	} */
 }
--- errorHandling/operations.go
+++ errorHandling/operations.go
@@ -1,10 +1,41 @@
 package main
 
-func (slice ProductSlice) TotalPrice(category string) (total float64) {
+type CategoryError struct {
+	reqCategory string
+}
+
+func (e *CategoryError) Error() string {
+	return "Category" + e.reqCategory + "does not exist"
+}
+
+type ChanMessage struct {
+	Category string
+	Total    float64
+	*CategoryError
+}
+
+func (slice ProductSlice) TotalPrice(category string) (total float64, err *CategoryError) {
+	prodCount := 0
 	for _, p := range slice {
 		if p.Category == category {
 			total += p.Price
+			prodCount++
 		}
 	}
+	if prodCount == 0 {
+		err = &CategoryError{reqCategory: category}
+	}
 	return
+}
+
+func (slice ProductSlice) TotalPriceAsync(categories []string, priceChannel chan<- ChanMessage) {
+	for _, c := range categories {
+		total, err := slice.TotalPrice(c)
+		priceChannel <- ChanMessage{
+			Category:      c,
+			Total:         total,
+			CategoryError: err,
+		}
+	}
+	close(priceChannel)
 }
