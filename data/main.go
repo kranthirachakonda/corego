2b12c12eb00cdf8a465cadd654e4f865e9fda7a4
Kranthi Rachakonda <kranthikumar.rachakonda@lowes.com>
2022-04-02 14:36:05 -0400

    Read/Write ops on file sample

--- data/main.go
+++ data/main.go
@@ -5,11 +5,12 @@ import (
 	"strings"
 )
 
-func readData(r io.Reader) {
+func processData(r io.Reader, w io.Writer) {
 	bSlice := make([]byte, 2)
 	for {
 		count, err := r.Read(bSlice)
 		if count > 0 {
+			w.Write(bSlice[0:count])
 			MyPrint("Read %v bytes: %v", count, string(bSlice[0:count]))
 		}
 		if err == io.EOF {
@@ -18,9 +19,23 @@ func readData(r io.Reader) {
 	}
 }
 
+func copyData(r io.Reader, w io.Writer) {
+	count, err := io.Copy(w, r)
+	if err == nil {
+		MyPrint("Copied bytes: %v", count)
+	} else {
+		MyPrint("Err copying %v", err.Error())
+	}
+}
+
 func main() {
 	MyPrint("Product: %v, Price: %v", Kayak.Name, Kayak.Price)
 
 	r := strings.NewReader("krachakonda")
-	readData(r)
+	var builder strings.Builder
+	processData(r, &builder)
+	MyPrint("strings builder content: %s", builder.String())
+	cr := strings.NewReader("copystring")
+	copyData(cr, &builder)
+	MyPrint("copied data: %s", builder.String())
 }
